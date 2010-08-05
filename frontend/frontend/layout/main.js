d66287122ce9469e665198d606502c0037c96274
Noah Zoschke <nzoschke@gmail.com>
2010-08-05 09:57:25 -0700

    shorten data set, clean up view def

--- frontend/frontend/layout/main.js
+++ frontend/frontend/layout/main.js
@@ -1,8 +1,8 @@
 include('../layout.js');
 
 frontend.layout.main = function() {
-  var data = ['this is', '30000k long', 'list'];
-  for (var i=3; i < 30000; i++) {
+  var data = ['this is', '300 long', 'list'];
+  for (var i=3; i < 300; i++) {
     data[i] = 'item #' + (i+1);
   };
   
@@ -12,23 +12,31 @@ frontend.layout.main = function() {
     background: '#EEE',
     childViews: [{
       view: 'ScrollableList', id: 'playlists',
-      rect: '200 640', anchors: 'left top bottom', 
-      data: data, rowHeight: 30, id: 'list', throttle: 0, multiselect: true, textSelectable: false
+      rect: '200 640', anchors: 'left top bottom',
+      rowHeight: 30,
+      throttle: 0, multiselect: true, textSelectable: false,
+      data: data
     },
     {
       view: 'ScrollableList', id: 'artists',
-      rect: '200 0 160 640', anchors: 'left top bottom', 
-      data: data, rowHeight: 30, id: 'list', throttle: 0, multiselect: true, textSelectable: false
+      rect: '200 0 160 640', anchors: 'left top bottom',
+      rowHeight: 30,
+      throttle: 0, multiselect: true, textSelectable: false,
+      data: data
     },
     {
       view: 'ScrollableList', id: 'albums',
-      rect: '360 0 160 640', anchors: 'left top bottom', 
-      data: data, rowHeight: 30, id: 'list', throttle: 0, multiselect: true, textSelectable: false
+      rect: '360 0 160 640', anchors: 'left top bottom',
+      rowHeight: 30,
+      throttle: 0, multiselect: true, textSelectable: false,
+      data: data
     },
     {
       view: 'ScrollableList', id: 'playlist',
-      rect: '520 0 440 640', anchors: 'left top bottom', 
-      data: data, rowHeight: 30, id: 'list', throttle: 0, multiselect: true, textSelectable: false
+      rect: '520 0 440 640', anchors: 'left top bottom',
+      rowHeight: 30,
+      throttle: 0, multiselect: true, textSelectable: false,
+      data: data
     }]
   });
 }
