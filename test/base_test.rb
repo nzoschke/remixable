b0e206f8cc7f34efb80bcc3831c3151884e7655f
Noah Zoschke <nzoschke@gmail.com>
2010-08-06 11:40:45 -0700

    always return all libraries

--- test/base_test.rb
+++ test/base_test.rb
@@ -37,7 +37,7 @@ class BaseTest < Test::Unit::TestCase
 
     noah.filtered.update("libraries" => ['noah'])
     assert_equal({ "libraries" => ['noah'], "playlists" => nil, "artists" => nil, "albums" => nil, "songs" => nil }, noah.filtered.filters)
-    assert_equal 1,  noah.filtered.libraries.count
+    assert_equal 2,  noah.filtered.libraries.count
     assert_equal 800, noah.filtered.songs.count
     assert noah.data
 
